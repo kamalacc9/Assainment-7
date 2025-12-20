8b3f97b399b5dd5f3c5a26df6ce1bc1c284dd373
kamalacc9 <mdkamalhossanacc9@gmail.com>
2025-12-21 03:10:41 +0600

    complete data card and task section design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import Navbar from "./Component/Navbar";
 import States from "./states";
-
+import DataContainer from "./DataContainer";
+import { Suspense } from "react";
+const loadData = () => fetch("/Data.json").then((res) => res.json());
 function App() {
+  const dataPromise = loadData();
+  console.log(dataPromise);
   return (
     <>
       <header className="  mx-auto py-3  lg:shadow-lg  ">
         <Navbar></Navbar>
       </header>
       <section>
-        <States></States>
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <DataContainer promise={dataPromise}></DataContainer>
+        </Suspense>
       </section>
     </>
   );
